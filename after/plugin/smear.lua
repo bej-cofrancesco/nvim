58b48eedace8251312fd7cb7f9d016cf3ac31cbf
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2025-04-01 14:31:51 +1100

    feat: created a trailing fire cursour

---
+++ after/plugin/smear.lua
@@ -0,0 +1,8 @@
+require("smear_cursor").setup({
+    cursor_color = "#33f1ff",
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    trailing_exponent = 5,
+    hide_target_hack = true,
+    gamma = 1,
+})
