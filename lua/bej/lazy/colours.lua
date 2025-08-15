57b1920bf894eaf626ceea21083ad5e095da8fce
Benjamin Cofrancesco <ben@flux.agency>
2025-08-15 23:08:25 +1000

    feat: updated to use rose pine

--- lua/bej/lazy/colours.lua
+++ lua/bej/lazy/colours.lua
@@ -1,13 +1,10 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			variant = "moon",
+		})
+		vim.cmd("colorscheme rose-pine")
+	end
 }
